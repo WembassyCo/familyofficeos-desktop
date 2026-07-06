48ee10c7c63f32fb2241036b967328051f1181d5
Chris McIntosh <cmcintosh@wembassy.com>
2026-07-06 11:40:50 -0500

    Add electron.vite.config.ts: Configuration for Electron build

---
+++ electron.vite.config.ts
@@ -0,0 +1,44 @@
+import { defineConfig, externalizeDepsPlugin } from 'electron-vite/utils'
+import { resolve } from 'path'
+import react from '@vitejs/plugin-react'
+
+export default defineConfig({
+  main: {
+    plugins: [externalizeDepsPlugin()],
+    build: {
+      rollupOptions: {
+        input: {
+          index: resolve(__dirname, 'src/main/index.ts')
+        }
+      }
+    }
+  },
+  preload: {
+    plugins: [externalizeDepsPlugin()],
+    build: {
+      rollupOptions: {
+        input: {
+          index: resolve(__dirname, 'src/preload/index.ts')
+        }
+      }
+    }
+  },
+  renderer: {
+    resolve: {
+      alias: {
+        '@': resolve(__dirname, 'src/renderer'),
+        '@main': resolve(__dirname, 'src/main'),
+        '@preload': resolve(__dirname, 'src/preload'),
+        '@shared': resolve(__dirname, 'src/shared')
+      }
+    },
+    plugins: [react()],
+    build: {
+      rollupOptions: {
+        input: {
+          index: resolve(__dirname, 'src/renderer/index.html')
+        }
+      }
+    }
+  }
+})
