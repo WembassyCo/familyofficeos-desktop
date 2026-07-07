02e97f9dbfceff23806cbcf5ec621181f4e45b55
Chris McIntosh <cmcintosh@wembassy.com>
2026-07-06 20:29:59 -0500

    fix: Add vite.config.ts for standard vite build

---
+++ vite.config.ts
@@ -0,0 +1,26 @@
+import { defineConfig } from 'vite'
+import { resolve } from 'path'
+import react from '@vitejs/plugin-react'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  base: './',
+  build: {
+    outDir: 'out/renderer',
+    emptyOutDir: true,
+    rollupOptions: {
+      input: {
+        index: resolve(__dirname, 'src/renderer/index.html')
+      }
+    }
+  },
+  resolve: {
+    alias: {
+      '@': resolve(__dirname, 'src/renderer'),
+      '@main': resolve(__dirname, 'src/main'),
+      '@preload': resolve(__dirname, 'src/preload'),
+      '@shared': resolve(__dirname, 'src/shared')
+    }
+  }
+})
